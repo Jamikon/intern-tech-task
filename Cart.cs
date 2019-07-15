85284b0ab52f0f9277d159d7e424321623d610ed
Jamikon <43286344+Jamikon@users.noreply.github.com>
2019-07-15 23:21:37 +1000

    added add/remove functionality
    improved old code; added add/remove fx.

--- Cart.cs
+++ Cart.cs
@@ -8,7 +8,7 @@ namespace ecommerce
     /// <summary>
     /// shopping cart class
     /// </summary>
-    class Cart
+    public class Cart
     {
         private int cartID, userID;
         private List<Product> products; // could probably use something a bit less resource intensive
@@ -19,11 +19,31 @@ namespace ecommerce
         /// </summary>
         /// <param name="userID"> userID that owns this cart </param>
         /// <param name="cartID"> cartID of this cart </param>
-        public Cart(int userID, int cartID = 0)
+        public Cart(int UID, int CID = 0)
         {
-            this.cartID = cartID;
-            this.userID = userID;
-            products = new List<Product>();
+            try
+            {
+                // IDs cant be negative
+                if (CID < 0 | UID < 0) throw new IndexOutOfRangeException();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("something went wrong instantiating Cart.");
+                Console.WriteLine(e);
+
+                Console.WriteLine("default params used to create cart instead");
+
+                // defaults could be set dynamically, instead of hardcoded
+                // as hardcoded default kinda defeats purpose of userID/cartID relationship
+                CID = -1;
+                UID = -1;
+            }
+            finally
+            {
+                this.cartID = CID;
+                this.userID = UID;
+                products = new List<Product>();
+            }
         }
 
 
@@ -32,6 +52,104 @@ namespace ecommerce
         /// </summary>
         ~Cart()
         {
+
+        }
+
+        /// <summary>
+        /// add product to this cart
+        /// </summary>
+        /// <param name="product"> product to add to this cart </param>
+        public void addProduct(Product product)
+        {
+            try
+            {
+                // product cant be null
+                if (product == null) throw new ArgumentNullException(); // cant have null product added
+
+
+                products.Add(product);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed to add product.\n");
+                Console.WriteLine(e);
+            }
+        }
+
+        /// <summary>
+        /// removes 1st occurrence of specified product
+        /// </summary>
+        /// <param name="product"> product to remove from cart </param>
+        public void removeProduct(Product product)
+        {
+            try
+            {
+
+                // products list does not contain product
+                // a bit redundant since try/catch will handle it either way regardless of this assertion
+                if (!products.Contains(product)) throw new ArgumentOutOfRangeException();
+
+                products.Remove(product);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed to remove product.\n");
+                Console.WriteLine(e);
+            }
+        }
+
+        /// <summary>
+        /// returns sum of products in this cart
+        /// </summary>
+        /// <returns> sum of products in this cart </returns>
+        public int numberOfProducts()
+        {
+            // no need for error handling since list cant be set to null and is always
+            // initialised at construction
+
+            return products.Count;
+        }
+
+        /// <summary>
+        /// returns current products
+        /// </summary>
+        /// <returns> products stored in this cart </returns>
+        public List<Product> GetProducts()
+        {
+            // no need for error handling since list is always initialised
+
+            return new List<Product>(products);
+        }
+
+        /// <summary>
+        /// returns a string representing contents of cart
+        /// </summary>
+        /// <returns> a string that represents contents of cart, including cart ID and user ID </returns>
+        public string cartInfoInString()
+        {
+            // no need for error handling since assertions on functions prevent addition of erroneous data
+
+
+            string str = "", tab = "\t|";    // str = output
+            int sum = numberOfProducts();   // in case more products added/removed before this task completes fully
+
+
+            str += "Cart ID: " + cartID + "\nUser ID of Cart: " + userID + "\nItems In Cart: " + sum;
+            str += "\nItems:" +
+                   "\n\n\tNumber\t|PID\t|Name";
+            str += "\n\t........|.......|..........................................................\n";
+
+            int i = 1;
+
+            // add each product
+            foreach (Product product in products)
+            {
+                str += "\t" + i + tab + product.productID + tab + product.name + "\n";
+                i++;
+            }
+
+
+            return str + "\n- end of cart -";
 
         }
 
--- Product.cs
+++ Product.cs
@@ -8,7 +8,16 @@ namespace ecommerce
     /// <summary>
     /// placeholder class. exists to demonstrate that Cart class will contain a list of these (product)
     /// </summary>
-    class Product
+    public class Product
     {
+        // public for now since this is out of the scope of the project and only
+        // for demonstrative purposes
+        public int productID;
+        public string name;
+        public Product(int id = 123, string name = "product name")
+        {
+            productID = id;
+            this.name = name;
+        }
     }
 }
