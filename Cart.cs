b308fa63413ede691c79d71e045e4a8d3d1e7b2f
Jamikon <43286344+Jamikon@users.noreply.github.com>
2019-07-15 18:44:31 +1000

    added shopping cart

---
+++ Cart.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ecommerce
+{
+    /// <summary>
+    /// shopping cart class
+    /// </summary>
+    class Cart
+    {
+        private int cartID, userID;
+        private List<Product> products; // could probably use something a bit less resource intensive
+
+        /// <summary>
+        /// constructor. fills out userID and cartID. cartID default value should be dynamic, but
+        /// since there is nowhere to get a running cartID number from, i will leave it hardcoded...
+        /// </summary>
+        /// <param name="userID"> userID that owns this cart </param>
+        /// <param name="cartID"> cartID of this cart </param>
+        public Cart(int userID, int cartID = 0)
+        {
+            this.cartID = cartID;
+            this.userID = userID;
+            products = new List<Product>();
+        }
+
+
+        /// <summary>
+        /// destructor. nothing to destruct since GC takes care of everything used so far...
+        /// </summary>
+        ~Cart()
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ecommerce
+{
+    /// <summary>
+    /// placeholder class. exists to demonstrate that Cart class will contain a list of these (product)
+    /// </summary>
+    class Product
+    {
+    }
+}
